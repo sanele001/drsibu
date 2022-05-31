25e870610413b9cccdf34929c55c585f18855187
sanele001 <sibiyas@outlook.com>
2022-05-31 03:19:20 +0200

    working on calendar screen

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import * as Font from "expo-font";
+import { useFonts } from "expo-font";
+
+import { StyleSheet, Text, View } from "react-native";
+
+import Mycalendar from "./screens/calender";
+import Home from "./screens/home";
 
 export default function App() {
+  useEffect(() => {
+    loadFonts();
+  });
+
+  async function loadFonts() {
+    await Font.loadAsync({
+      // Load a font `Montserrat` from a static resource
+      Poppins: require("./assets/Poppins-Light.ttf"),
+    });
+  }
+
+  let [fontsLoaded] = useFonts({
+    "Poppins-Light": require("./assets/Poppins-Light.ttf"),
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Home />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
